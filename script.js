c6e71c7256a819111c127b5e1fd2c874663847ca
Marvin Jakobs <38141141+OfficialMarvin@users.noreply.github.com>
2024-04-13 17:07:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,32 @@
-// Chip-8 Games
-const games = {
-  'PONG': 'https://this.st/pong.ch8',
-  'TETRIS': 'https://this.st/tetris.ch8',
-  'SPACE INVADERS': 'https://this.st/invaders.ch8',
-  'BRIX': 'https://this.st/brix.ch8',
-  'MAZE': 'https://this.st/maze.ch8',
-  'MISSILE': 'https://this.st/missile.ch8',
-  'GUESS': 'https://this.st/guess.ch8',
-  'CONNECT 4': 'https://this.st/connect4.ch8',
-  'PONG 2': 'https://this.st/pong2.ch8',
-  'TANK': 'https://this.st/tank.ch8',
-  'UFO': 'https://this.st/ufo.ch8',
-  'WIPEOUT': 'https://this.st/wipeout.ch8',
-  'ASTEROIDS': 'https://this.st/asteroids.ch8',
-  'BREAKOUT': 'https://this.st/breakout.ch8',
-  'SERIES': 'https://this.st/series.ch8',
-  'HIDDEN': 'https://this.st/hidden.ch8',
-  'MISSILE COMMAND': 'https://this.st/missile_command.ch8',
-  'DEFENDER': 'https://this.st/defender.ch8',
-  'VBRIX': 'https://this.st/vbrix.ch8',
-  'CAVE': 'https://this.st/cave.ch8',
-  'TANK WAR': 'https://this.st/tank_war.ch8',
-  'INVADERS': 'https://this.st/invaders.ch8',
-  'COUNTDOWN': 'https://this.st/countdown.ch8',
-  'MERLIN': 'https://this.st/merlin.ch8',
-  'KALEID': 'https://this.st/kaleid.ch8',
-  'TRON': 'https://this.st/tron.ch8',
-  'MISSILE COMMAND 2': 'https://this.st/missile_command_2.ch8',
-  'BLITZ': 'https://this.st/blitz.ch8',
-  'FLOOD': 'https://this.st/flood.ch8',
-  'GUNFIGHT': 'https://this.st/gunfight.ch8',
-  'KEYPAD': 'https://this.st/keypad.ch8',
-  'SPADE': 'https://this.st/spade.ch8',
-  'VERS': 'https://this.st/vers.ch8',
-  'ANIMAL RACE': 'https://this.st/animal_race.ch8',
-  'SPACEFIGHT': 'https://this.st/spacefight.ch8',
-  'OXXO': 'https://this.st/oxxo.ch8',
-  'BLINKEN': 'https://this.st/blinken.ch8'
-};
-
-// Function to load and play a Chip-8 game
-async function playGame(gameName) {
-  const gameUrl = games[gameName];
-  if (!gameUrl) {
-    console.error(`Game "${gameName}" not found.`);
-    return;
+// Abstract CPU interface class
+class CpuInterface {
+  constructor() {
+    if (new.target === CpuInterface) {
+      throw new TypeError('Cannot instantiate abstract class');
+    }
   }
 
-  try {
-    const response = await fetch(gameUrl);
-    const romBuffer = new RomBuffer(await response.arrayBuffer());
-    const cpu = new CPU(new MockCpuInterface());
-    cpu.load(romBuffer);
+  clearDisplay() {
+    throw new TypeError('Must be implemented on the inherited class.');
+  }
 
-    while (true) {
-      cpu.step();
-    }
-  } catch (error) {
-    console.error(`Error playing game "${gameName}": ${error}`);
+  waitKey() {
+    throw new TypeError('Must be implemented on the inherited class.');
+  }
+
+  getKeys() {
+    throw new TypeError('Must be implemented on the inherited class.');
+  }
+
+  drawPixel() {
+    throw new TypeError('Must be implemented on the inherited class.');
+  }
+
+  enableSound() {
+    throw new TypeError('Must be implemented on the inherited class.');
+  }
+
+  disableSound() {
+    throw new TypeError('Must be implemented on the inherited class.');
   }
 }
-
-// Example usage
-playGame('PONG');
